a72b9e0d922236a314bc7284a1b859ed257939c5
OlehOpara <olegopara20@gmail.com>
2023-01-31 16:48:26 +0100

    new commit

--- js/script.js
+++ js/script.js
@@ -1,88 +1,117 @@
-'use strict';
+let startBtn = document.querySelectorAll('#start'),
+    budgetValue = document.getElementsByClassName('budget-value')[0],
+    dayBudgetValue = document.getElementsByClassName('daybudget-value')[0],
+    levelValue = document.getElementsByClassName('level-value')[0],
+    expensesValue = document.getElementsByClassName('expenses-value')[0],
+    optionalExpensesValue = document.getElementsByClassName('optionalexpenses-value')[0],
+    incomeValue = document.getElementsByClassName('income-value')[0],
+    monthSavingsValue = document.getElementsByClassName('monthsavings-value')[0],
+    yearSavingsValeue = document.getElementsByClassName('yearsavings-value')[0],
+
+    expensesItem = document.getElementsByClassName('expenses-item'),
 
 
-let money = +prompt("Ваш бюджет на месяц?", ""),
-    time = prompt("Введите дату в формате YYYY-MM-DD", "");
+    expensesItemBtn = document.getElementsByTagName('button')[0],
+    optionalExpensesItemBtn = document.getElementsByTagName('button')[1],
+    countBudgetBtn = document.getElementsByTagName('button')[2],
 
+    optionalExpensesItem = document.querySelectorAll('.optionalexpenses-item'),
+
+    incomeItem = document.querySelector('.choose-income'),
+    checkSavings = document.querySelector('#savings'),
+    sumValue = document.querySelector('.choose-sum'),
+    percentValue = document.querySelector('.choose-percent'),
+    yearValue = document.querySelector('.year-value'),
+    monthValue = document.querySelector('.month-value'),
+    dayValue = document.querySelector('.day-value');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let money, time;
+
+
+function start() {
+    money = +prompt('Ваш бюджет на месяц', '');
+    time = prompt('Введите дату в формате YYYY-MM-DD', '');
+    while (isNaN(money) || money == '' || money == null) {
+        money = +prompt('Ваш бюджет на месяц', '');
+
+
+    }
+}
+start();
 let appData = {
-    budget: money,
+    budjet: money,
     timeData: time,
     expenses: {},
     optionalExpenses: {},
     income: [],
-    savings: false
-};
+    saving: true,
+    chooseExpenses: function () {
+        for (let i = 0; i < 2; i++) {
+            let a = prompt('Введите обязательную статью расходов в этом месяце', ''),
+                b = prompt('Во сколько это обойдется', '');
+            if ((typeof (a)) === 'string' && (typeof (a) != null) && (typeof (b) != null) && a != '' && b != '' && a.length < 50) {
 
+                console.log('done');
+                appData.expenses[a] = b;
+            } else {
+                console.log('Произошла ошибка');
+            }
+        }
 
-for (let i = 0; i < 2; i++) {
-    let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
-        b = prompt("Во сколько обойдется?", "");
+    },
+    detectDayBudget: function () {
+        appData.moneyPerDay = (appData.budjet / 30).toFixed(1);
+        alert('Ежидневный бюждет' + appData.moneyPerDay);
+    },
+    detectLevel: function () {
+        if (appData.moneyPerDay < 100) {
+            console.log('минимальный уровень достатка');
+        } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 1000) {
+            console.log('средний уровень достатка');
+        } else if (appData.moneyPerDay > 1000) {
+            console.log('высокий уровень достатка');
+        } else {
+            console.log('произошла ошибка');
+        }
+    },
+    checkSavings: function () {
+        if (appData.saving == true) {
+            let save = +prompt('Какова сумма накоплений?'),
+                percent = +prompt('Под какой процент');
+            appData.monthIncome = save / 100 / 12 * percent;
+            alert('Доход в месяц с вашего депозита: ' + appData.monthIncome.toFixed());
 
-    if (typeof (a) === 'string' && typeof (a) != null && typeof (b) != null && a != "" && b != "" && a.length < 50) {
+        }
+    },
+    chooseOptExpenses: function () {
+        for (let i = 1; i <= 3; i++) {
+            let questionOptExpenses = prompt("Статья необязательных расходов?");
+            appData.optionalExpenses[i] = questionOptExpenses;
+            console.log(appData.optionalExpenses);
+        }
+    },
+    chooseIncome: function () {
+        let items = prompt('Что принесет дополнительный доход? (Перечислите через запятую)', '');
+        appData.income = items.split(',');
+        appData.inocme.push(prompt('Может что то еще?'));
+        appData.income.sort();
 
-        console.log("done");
-
-        appData.expenses[a] = b;
-    } else {
-        console.log("bad result");
-        i--;
     }
 
 };
 
-// let i = 0;
-// while (i < 2) {
-//     let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
-//         b = prompt("Во сколько обойдется?", "");
 
-//     if (typeof (a) === 'string' && typeof (a) != null && typeof (b) != null && a != "" && b != "" && a.length < 50) {
-
-//         console.log("done");
-
-//         appData.expenses[a] = b;
-//     } else {
-//         console.log("bad result");
-//         i--;
-//     }
-
-//     i++;
-// }
-
-
-// let i = 0;
-// do {
-//     let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
-//         b = prompt("Во сколько обойдется?", "");
-
-//     if (typeof (a) === 'string' && typeof (a) != null && typeof (b) != null && a != "" && b != "" && a.length < 50) {
-
-//         console.log("done");
-
-//         appData.expenses[a] = b;
-//     } else {
-//         console.log("bad result");
-//         i--;
-//     }
-//     i++;
-// }
-// while (i < 2);
-
-
-
-appData.moneyPerDay = appData.budget / 30;
-
-
-
-
-alert("Бюджет на 1 день составляет " + appData.moneyPerDay + "руб.");
-
-
-if (appData.moneyPerDay < 100) {
-    console.log("Это минимальный уровень достатка!");
-} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-    console.log("Это средний уровень достатка!");
-} else if (appData.moneyPerDay > 2000) {
-    console.log("Это высокий уровень достатка!");
-} else {
-    console.log("Произошла ошибка");
-}
